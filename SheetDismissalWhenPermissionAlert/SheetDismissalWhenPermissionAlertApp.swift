f9ca4b5c4063b35cab9af4633577ea3ce1951e8d
Tobias Kreß <tobias.kress@intive.com>
2023-07-14 11:05:04 +0200

    Initial Commit

---
+++ SheetDismissalWhenPermissionAlert/SheetDismissalWhenPermissionAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetDismissalWhenPermissionAlertApp.swift
+//  SheetDismissalWhenPermissionAlert
+//
+//  Created by Tobias Kreß on 14.07.23.
+//
+
+import SwiftUI
+
+@main
+struct SheetDismissalWhenPermissionAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
